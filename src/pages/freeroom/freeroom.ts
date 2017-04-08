3275ceabf82b9095853ac8b8ba9c6dfdc52ff8c0
ryansutc <ryansutc@gmail.com>
2017-04-08 14:11:37 -0300

    Find Room Combobox Dependency in Progress. Not Updating Buildings after campus implements unique List

--- src/pages/freeroom/freeroom.ts
+++ src/pages/freeroom/freeroom.ts
@@ -13,9 +13,9 @@ import {Building} from "../../app/building";
   //providers: [FreeRoomService] // Component requires FreeRoomService for data to put into page (dependency injection)
 })
 export class FreeRoomPage implements OnInit {
-  buildings: any; //Building[];
-  public campuses = [{campus:"AKERL", campusName:"Akerley"}, {campus:"ANNAP",campusName:"Annapolis Valley"}];
-
+  public buildings: Building[] = new Array<Building>();
+  public campuses;
+  public optionBuildings: Building[] = new Array<Building>();
   now: string;
   selCampus: string = "INSTI";
   selBuilding: string = "ITC";
@@ -25,7 +25,6 @@ export class FreeRoomPage implements OnInit {
   ngOnInit(): void {
 
     this.now = this.getNow().toString();
-    this.buildings = new Array<Building>();
     //Get returns results in form of an observable, map converts to JSON, subscribe allows access/iteration
     this.http.get('http://nsccsucks.xyz/RoomsList/getJSON').map(res => res.json()).subscribe(data => {
         let buildingList: Building[];
@@ -37,23 +36,35 @@ export class FreeRoomPage implements OnInit {
           buildingObj.buildingName = building.buildingName;
           this.buildings.push(buildingObj);
         }
-
+        this.campuses = this.getUniqueListOfCampuses();
       },
       err => {
         console.log("Oops!");
       });
   }
-  //Get Buildings by a CampusId
-  getBuildingsByCampus(campusId: string): Building[] {
+  //Get list of relevant Buildings by a CampusName
+  getBuildingsByCampus(campusName: string): Building[] {
     let newBuildings : Building[];
+    newBuildings = new Array<Building>();
     for (let i in this.buildings){
-      if(this.buildings[i].campusId == campusId){
+      if(this.buildings[i].campusName == campusName){
         newBuildings.push(this.buildings[i]);
       }
     }
     return newBuildings;
   }
 
+  getUniqueListOfCampuses(): Building[] {
+    let campusList: Building[] = new Array<Building>();
+    let simpleList = []; //just to help use its find method for a unique list
+    for( let i in this.buildings){
+      if(!simpleList.find(x => x == this.buildings[i].campusName)){
+        simpleList.push(this.buildings[i].campusName);
+        campusList.push(this.buildings[i]);
+      }
+    }
+    return campusList;
+  }
   getNow(): string {
     let time = moment().minutes();
     let mins = 0;
@@ -83,9 +94,14 @@ export class FreeRoomPage implements OnInit {
       hours = hours -12;
     }
     */
-    let now = hours.toString() + "" + mins + " " + moment().format(' A').toString();
     //return now;
     return moment().hour(hours).minutes(mins).format('hh:mm');
   };
 
+  //on Campus dropdown change event handler
+  CampusChange(value): void {
+    this.optionBuildings = this.getBuildingsByCampus(value);
+
+
+  }
 }
